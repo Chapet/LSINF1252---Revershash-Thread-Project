a89f3aee5a199d571f8ea5595a7cea6be8081522
Louis <louis99laurent@gmail.com>
2019-05-04 12:55:31 +0200

    Add includes, global variable (-c), function countOcc (+upd_candidate)

--- src/cracker.c
+++ src/cracker.c
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <errno.h>
+
 
 #include "sha256.h"
 #include "reverse.h"
@@ -14,25 +17,49 @@ typedef struct Candidate {
     char *password;
 }Candidate;
 
+char consonant_or_vowel = 'v';
+
+/* pre : str!=NULL, str is a string containing only letters ranging a-z
+ * post : returns the number of occurrences of vowel or consonant, depending on the global variable consonant_or_vowel;
+ */
+int countOcc (char* str) {
+    if (str == NULL) {
+        return 0;
+    }
+    char* cur = str; //a pointer to browse the string
+    int vowel = 0; //to count the number of vowel's occurrences
+    int i;
+// ou alors c'est plus propre de faire while (*cur != %0) ?
+    for (i = 0; i < strlen(str); i++) { //browsing the string
+        if (*(cur+i) == 'a' || *(cur+i) == 'e' || *(cur+i) == 'i' || *(cur+i) == 'o' || *(cur+i) == 'u' || *(cur+i) == 'y') { //the current char is a vowel
+            vowel ++;
+        }
+    }
+
+    if(consonant_or_vowel == 'v')
+        return vowel;
+    else
+        return strlen(str)-vowel; //every char that is not a vowel is a consonant
+}
 
 /*pre : head != NULL, head->password != NULL, pwd != NULL
-  post :Checks if a password is the be added to the list of candidates by comparing his length to to the current candidates length.
-
-  If the password's length is greater, the current list is replaced with a list containing only this password as candidate.
-  Else if the password's length is identical, the password is added to the list of candidates
-  Else (the password's length is shorter), the password is not added and thus the list stays unchanged.*/
-void update_candidate(Candidate* head, int len, char* pwd) {
+ *post : Checks if a password is the be added to the list of candidates by comparing his length to to the current candidates length.
+ *
+ *If the password's length is greater, the current list is replaced with a list containing only this password as candidate.
+ *Else if the password's length is identical, the password is added to the list of candidates
+ *Else (the password's length is shorter), the password is not added and thus the list stays unchanged.*/
+void update_candidate(Candidate* head, int curOcc, char* pwd) { //len a remplacer par nbOcc (pr clarte)
     if (head == NULL || head->password == NULL || pwd == NULL){
         return;
     }
 
-    int pwdLen = strlen(pwd);
+    int pwdOcc = countOcc(pwd);
 
-    if (pwdLen > len) {
+    if (pwdOcc > curOcc) {
         head->next = NULL;
         head->password = pwd;
     }
-    else if (pwdLen == len) {
+    else if (pwdOcc == curOcc) {
         Candidate* newCand = malloc(sizeof(Candidate));
         *newCand = (Candidate){head->next, pwd};
         head->next = newCand;
@@ -65,16 +92,6 @@ int main(int argc, char *argv[]) {
     //ecriture
 
     printf("Test starto !\n");
-
-    char* p3 = "glinglin";
-    char* p4 = "glauglau";
-    char* p5 = "glouglou";
-    Candidate a = {NULL, p3};
-    Candidate* b = &a;
-
-    update_candidate(b, 8, p4);
-    update_candidate(b, 8, p5);
-    writeOutput(b);
 
     printf("Test complete !\n");
     return EXIT_SUCCESS;
